862c58b46504a68c856edaaee031a256bdfa9fc0
David <kdavid161262@yandex.ru>
2022-06-15 15:00:39 +0300

    First commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,83 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from "react";
+import { fetchFilms, fetchTop } from "./api/FetchFilms.jsx";
+import styles from "./css/App.module.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [films, setFilms] = useState([]);
+    const [isLoading, setIsLoading] = useState(false);
+
+    const getFilms = async (params) => {
+        try {
+            setIsLoading(true);
+            const res = await fetchFilms(params);
+            setFilms(res);
+        } catch (err) {
+            console.error(err);
+        } finally {
+            setIsLoading(false);
+        }
+    };
+
+    const getTop = async (params) => {
+        try {
+            setIsLoading(true);
+            const res = await fetchTop(params);
+            setFilms(res);
+        } catch (err) {
+            console.error(err);
+        } finally {
+            setIsLoading(false);
+        }
+    };
+
+    console.log(films);
+
+    useEffect(() => {
+        getFilms();
+    }, []);
+
+    // const get = () => {
+    //     useEffect(() => {
+    //         getTop();
+    //     }, []);
+    // };
+
+    console.log(films);
+
+    return (
+        <div className="container">
+            <div className={styles.btns}>
+                <div>
+                    <h1>Фильтры:</h1>
+                </div>
+                <div>
+                    <button className={styles.btn} onClick={() => getTop()}>
+                        Топ 250 фильмов
+                    </button>
+                    <button className={styles.btn}>Популярные</button>
+                    <button className={styles.btn}>Новые</button>
+                </div>
+            </div>
+            <h2 className={styles.all}>Все фильмы:</h2>
+            <div className={styles.cards}>
+                {isLoading ? (
+                    <div>Loading...</div>
+                ) : (
+                    films?.data?.items.map((film) => (
+                        <div className={styles.card} key={film.kinopoiskId}>
+                            <h2 className={styles.title}>{film.nameRu}</h2>
+                            <img
+                                width={"150px"}
+                                height={"150px"}
+                                src={film.posterUrl}
+                                alt="description"
+                            />
+                        </div>
+                    ))
+                )}
+            </div>
+        </div>
+    );
 }
 
 export default App;
